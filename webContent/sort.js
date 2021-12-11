c0c1a0fef126ec3131c48c4dce2536ff787f532a
Mason Gobat <masongobat@gmail.com>
2021-12-11 17:45:52 -0500

    Optimized for JS

---
+++ webContent/sort.js
@@ -0,0 +1,24 @@
+"use strict";
+
+const checkboxes = document.querySelectorAll('details > input');
+window.addEventListener('DOMContentLoaded', (event) => {
+    /* Attach event listeners to checkboxes */
+    for (var i = 0; i < checkboxes.length; i++) {
+        checkboxes[i].addEventListener('change', filterInsults, false)
+    }
+});
+function filterInsults() {
+    //Get the @id values of the checkboxes that are unchecked
+    var unchecked_boxes_array = Array. from (checkboxes).filter(item => ! item.checked).map(item => item.id);    
+    /* Find all insults (ins)
+     * Hide each insult (momentarily)
+     * Show each insult that contains an checked @id  value
+     *  */
+    const insults = document.getElementsByClassName('insult');
+    for (var i = 0; i < insults.length; i++) {
+        insults[i].style.display = 'none';
+        if (Array. from (poems[i].classList).some(item => ! unchecked_boxes_array.includes(item))) {
+            insults[i].style.display = 'block';
+        }
+    }
+}
